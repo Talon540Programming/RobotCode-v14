8c93ef5df7c9c464138dd54931fbfc030173b4e0
Bluewaves54 <88750406+Bluewaves54@users.noreply.github.com>
2022-01-18 15:02:42 -0500

    Update forceCalculations.py

--- Raspberry Pi Scripts/forceCalculations.py
+++ Raspberry Pi Scripts/forceCalculations.py
@@ -1,17 +1,19 @@
 import math
 
-hubStackHeight = 109/8 #Feet
-LimelightSensorHeight = 13/3 #Feet
+def shooterCalculations(distanceFromTopHub):
+  hubStackHeight = 109/8 #Feet
+  LimelightSensorHeight = 13/3 #Feet
 
-# Get Limelight Data
-# Convert to Feet
-distanceFromTopHub = 15 #Feet
+  # Get Limelight Data
+  # Convert to Feet
 
-hubStackHeight = hubStackHeight-LimelightSensorHeight
-distanceFromHubBase = math.sqrt(math.pow(distanceFromTopHub,2)-math.pow(hubStackHeight,2))
-rawShooterAngle = math.asin((hubStackHeight)/(distanceFromTopHub))
+  hubStackHeight = hubStackHeight-LimelightSensorHeight
+  distanceFromHubBase = math.sqrt(math.pow(distanceFromTopHub,2)-math.pow(hubStackHeight,2))
+  rawShooterAngle = math.asin((hubStackHeight)/(distanceFromTopHub))
 
-# Add physics and calculus here. Update angle. Get speed
+  # Add physics and calculus here. Update angle. Get speed
 
-print("Shooter Angle: "+str(rawShooterAngle))
-print("Distance from Hubs: "+str(distanceFromHubBase))
+  print("Shooter Angle: "+str(rawShooterAngle))
+  print("Distance from Hubs: "+str(distanceFromHubBase))
+  
+  return (rawShooterAngle, distanceFromHubBase)
