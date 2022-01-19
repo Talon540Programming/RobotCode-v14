462183844c6ca018f280f6d0482e0bde3953ef39
Bluewaves54 <88750406+Bluewaves54@users.noreply.github.com>
2022-01-18 23:54:47 -0500

    Update ballRecogniton.py

--- Raspberry Pi Scripts/ballRecogniton.py
+++ Raspberry Pi Scripts/ballRecogniton.py
@@ -4,57 +4,42 @@ import time
 import numpy as np
 import datetime
 from threading import Thread
-
-#creating frame updating class
+# defining class for frame update
 class FPS:
     def __init__(self):
         self._start = None
         self._end = None
         self._numFrames = 0
-
     def start(self):
         self._start = datetime.datetime.now()
         return self
-
     def stop(self):
         self._end = datetime.datetime.now()
-
     def update(self):
         self._numFrames += 1
-
     def elapsed(self):
         return (self._end - self._start).total_seconds()
-
     def fps(self):
         return self._numFrames / self.elapsed()
-
-#creating webcam class for multithreading - faster fps
+# defining class for threading VideoCapture frame read
 class WebcamVideoStream:
     def __init__(self, src=0):
         self.stream = cv2.VideoCapture(src)
         (self.grabbed, self.frame) = self.stream.read()
         self.stopped = False
-
     def start(self):
         Thread(target=self.update, args=()).start()
         return self
-
     def update(self):
         while True:
             if self.stopped:
                 return
-
             (self.grabbed, self.frame) = self.stream.read()
-
     def read(self):
         return self.frame
-
     def stop(self):
         self.stopped = True
-
-
 # initialize video capture
-cap = cv2.VideoCapture(0)
 fps = FPS().start()
 vs = WebcamVideoStream(src=0).start()
 # wait for camera to start
@@ -62,12 +47,12 @@ time.sleep(2)
 # set color boundaries
 lower_blue, upper_blue = np.array([100, 35, 140]), np.array([180, 255, 255])
 lower_red, upper_red = np.array([0, 50, 50]), np.array([10, 255, 255])
-
-
 def detectBalls():
-    global cap, fps, lower_blue, upper_blue, lower_red, upper_red
+    global fps, lower_blue, upper_blue, lower_red, upper_red
+    start = time.perf_counter()
+    stop = time.perf_counter()
     # intialize updating while loop
-    while cap.isOpened():
+    while stop - start < 30:
         # reading video capture frame
         frame = vs.read()
         centerOfScreen = frame.shape[1] / 2
@@ -83,7 +68,6 @@ def detectBalls():
         # finding contour extremeties from mask
         cnts_blue, _ = cv2.findContours(mask_blue.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
         cnts_red, _ = cv2.findContours(mask_red.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
-
         centers_blue = []
         distances_from_center_blue = []
         centers_red = []
@@ -111,10 +95,8 @@ def detectBalls():
                     continue
                 if min_distance < 0:
                     print('turn left {}'.format(abs(min_distance)))
-                    time.sleep(1)
                 else:
                     print('turn right {}'.format(min_distance))
-                    time.sleep(1)
             else:
                 continue
         # for red regions
@@ -132,17 +114,14 @@ def detectBalls():
                     cv2.circle(result, centers_red[-1], 5, (0, 255, 255), -1)
         # showing current frame
         cv2.imshow("Result", result)
-
         # if q is pressed, the while loop breaks
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
+        stop = time.perf_counter()
         fps.update()
     # end video capture and destroy all video feeds
     fps.stop()
     print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
     print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-    cap.release()
+    vs.stop()
     cv2.destroyAllWindows()
-
-detectBalls()
