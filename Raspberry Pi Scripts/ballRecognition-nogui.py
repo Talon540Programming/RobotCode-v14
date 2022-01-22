fd03d658fa4e567400ac876ec2589b69c7511ea2
Spirit-Supreme <68172138+Spirit-Supreme@users.noreply.github.com>
2022-01-22 12:11:11 -0500

    Update ballRecognition-nogui.py

--- Raspberry Pi Scripts/ballRecognition-nogui.py
+++ Raspberry Pi Scripts/ballRecognition-nogui.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
 
-print("Hello World")
-print("Hello World")
+print("[Imported OpenCV] Current Version: "+cv2.__version__)
+print("[Imported numpy] Current Version: "+np.__version__)
+
+print("Ayush Pal is gay")
