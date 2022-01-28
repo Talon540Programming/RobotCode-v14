5606085b3fadefe20b73271d368bbb4fdbb8c1c8
Sriman Achanta <68172138+Spirit-Supreme@users.noreply.github.com>
2022-01-28 11:37:10 -0500

    Update angleModule.java

--- TestingModules/angleModule.java
+++ TestingModules/angleModule.java
@@ -2,12 +2,14 @@ import java.util.Scanner;
 class Main {
     // Height to top of hubs in Meters from the floor
     private static final double HubHeight = 2.64;
-    // Height to Limelight from floor in meters
+    // Height to Limelight from floor in meters on 13.0
     private static final double LimelightHeight = 0.4882896; //meters
     // Height difference between Hubs and Limelight
     private static final double HEIGHTDIFFERENCE = HubHeight-LimelightHeight;; // Height difference between shooter/Limelight and HubStackHeight
     // Angle of Limelight to the ground (63° on 13.0, used for testing)
     private static final double LimelightFixedAngle = 63 ; // Fixed Limelight Angle
+    // Angle of Entry into the hub
+    private static final double IdealEntryAngle = -20;
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -16,11 +18,6 @@ class Main {
         long startNano = System.nanoTime();
         double distance = 0;
 
-        // if(LimelightFluidAngle < 0) {
-        //     distance = 10;
-        // } else {
-        //     distance = HEIGHTDIFFERENCE/(Math.tan(((Math.toRadians(LimelightFixedAngle))+(Math.toRadians(LimelightFluidAngle)))));
-        // }
         distance = HEIGHTDIFFERENCE/(Math.tan(((Math.toRadians(LimelightFixedAngle))+(Math.toRadians(LimelightFluidAngle)))));
 
         System.out.println("Distance from Hubs: "+distance);
@@ -34,7 +31,7 @@ class Main {
     }
 
     public static double calculateAngle(double d) { // Takes distance between shooter and the hub (will be from limelight)
-        return Math.toDegrees(Math.atan((Math.tan(Math.toRadians(-20))*(d)-(2*HEIGHTDIFFERENCE))/(-d)));
+        return Math.toDegrees(Math.atan((Math.tan(Math.toRadians(IdealEntryAngle))*(d)-(2*HEIGHTDIFFERENCE))/(-d)));
         // Return the necessary angle the shooter should actuate to
     }
 
