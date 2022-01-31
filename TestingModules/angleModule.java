fd00aa67cf93cd0d65a02173b3e6c4aa5e275c4a
Spirit-Supreme <68172138+Spirit-Supreme@users.noreply.github.com>
2022-01-30 19:06:29 -0500

    Revert "a"
    This reverts commit c5119f4acbd71c4a111caa83f5db9cd3c912530d.

--- TestingModules/angleModule.java
+++ TestingModules/angleModule.java
@@ -14,7 +14,6 @@ class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter ⍬ (from LimeLight) in Degrees: ");
-    
         double LimelightFluidAngle = scan.nextDouble();
         long startNano = System.nanoTime();
         double distance = 0;
@@ -32,7 +31,6 @@ class Main {
         long elapsedTimeNano = finishNano - startNano; 
         //Time for the Calculation to complete
         System.out.println("Calculation Time: "+elapsedTimeNano/1000000+" ms");
-        scan.close();
     }
 
     public static double calculateAngle(double d) { // Takes distance between shooter and the hub (will be from limelight)
