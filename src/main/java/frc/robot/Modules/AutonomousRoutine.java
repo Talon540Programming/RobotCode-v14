1af6717f83649c703cb6fd755d43bf5d8fc9d12c
aryanmhaskar <63806946+aryanmhaskar@users.noreply.github.com>
2022-03-15 11:44:39 -0400

    Created Separate File for Auto Routine
    -Uses Gyro Commands for Steering Correction.
    -Uses Encoders for Odometry.

---
+++ src/main/java/frc/robot/Modules/AutonomousRoutine.java
@@ -0,0 +1,11 @@
+package frc.robot.Modules;
+
+import frc.robot.Robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class AutonomousRoutine {
+  
+}
