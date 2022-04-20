2918903bba7b04c3b0829d0ec6e5f17458e4e2ad
AayuOSh123 <74684221+AayuOSh123@users.noreply.github.com>
2022-04-20 16:26:40 -0400

    Update MotorControl.java

--- src/main/java/frc/robot/Modules/MotorControl.java
+++ src/main/java/frc/robot/Modules/MotorControl.java
@@ -15,6 +15,7 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.XboxController;
 
 
 public class MotorControl {
