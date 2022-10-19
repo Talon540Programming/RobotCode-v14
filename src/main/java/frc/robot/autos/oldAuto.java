2c4e1fd836def5f869dfa7f5c92a8470c1d8abfb
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-10-18 21:28:36 -0400

    re wrote auto command to be more readable for testing

--- src/main/java/frc/robot/autos/oldAuto.java
+++ src/main/java/frc/robot/autos/oldAuto.java
@@ -4,14 +4,18 @@ import org.talon540.vision.Limelight.LimelightVision;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.constants.Measurements;
 import frc.robot.drivetrain.DrivetrainBase;
 import frc.robot.drivetrain.commands.CenterRobotOnHubStack;
 import frc.robot.drivetrain.commands.DriveToDistance;
 import frc.robot.shooter.ShooterBase;
-import frc.robot.shooter.commands.SingleBallFire;
+import frc.robot.shooter.commands.SetShooter;
+import frc.robot.shooter.commands.StopFlywheel;
 import frc.robot.wrist.WristBase;
 import frc.robot.wrist.commands.rollers.KickupBall;
+import frc.robot.wrist.commands.rollers.SetRollers;
+import frc.robot.wrist.commands.rollers.StopRollers;
 
 /**
  * Old Auto Sequence. Auto sequence used during the 2022, Rapid React
@@ -29,17 +33,33 @@ import frc.robot.wrist.commands.rollers.KickupBall;
 public class oldAuto extends SequentialCommandGroup {
     public oldAuto(DrivetrainBase drivetrainBase, ShooterBase shooterBase, WristBase wristBase, LimelightVision limelightBase) {
         addCommands(
+            // Center the robot on the hub stack
             new CenterRobotOnHubStack(drivetrainBase, limelightBase),
 
+            // Get to a good distance from the hubstack and rev the flywheel
             new ParallelCommandGroup(
                     new DriveToDistance(drivetrainBase, limelightBase, 1.3), // TODO: find distance
-                    new SingleBallFire(shooterBase)),
+                    new SequentialCommandGroup(
+                        new SetShooter(shooterBase, 1),
+                        new WaitCommand(2)
+                    )
+            ),
 
+            // Center the robot again to account for any error that occured during driving
             new CenterRobotOnHubStack(drivetrainBase, limelightBase),
 
-            new KickupBall(wristBase),
+            // Fire the ball from the trough and stop the flywheel and rollers after
+            new SequentialCommandGroup(
+                new SequentialCommandGroup(
+                    new SetRollers(wristBase, 0.5),
+                    new WaitCommand(3)
+                ),
 
-            new DriveToDistance(drivetrainBase, limelightBase,
-                    Measurements.Robot.botlengthBumpersMeters + Measurements.Field.tarmacLengthMeters));
+                new StopFlywheel(shooterBase),
+                new StopRollers(wristBase)
+            ),
+
+            // Taxi
+            new DriveToDistance(drivetrainBase, limelightBase, Measurements.Robot.botlengthBumpersMeters + Measurements.Field.tarmacLengthMeters));
     }
 }
