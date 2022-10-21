ef5ced02a591c250a7fe9d4895855b3d64e8b300
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-10-21 13:56:52 -0400

    added resistance checking to wrist, resolves #34

--- src/main/java/frc/robot/wrist/rotation/WristRotationBase.java
+++ src/main/java/frc/robot/wrist/rotation/WristRotationBase.java
@@ -2,62 +2,54 @@ package frc.robot.wrist.rotation;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.CANDeviceIDS;
 
 public class WristRotationBase extends SubsystemBase {
     private WPI_TalonFX wristRotation;
 
-    private int lastInput = 0;
-    private boolean wristDisabled = false;
+    public final double resistanceTollerance = 4.8;
 
     public WristRotationBase() {
         this.wristRotation = new WPI_TalonFX(CANDeviceIDS.INTAKE_WRIST);
     }
 
-    @Override
-    public void periodic() {
-        double resistance = getResistance();
+    /**
+     * Get resistance from the motor in amps.
+     * As a motor induces a higher load, it will draw more amperage. We can use this
+     * to our advantage by measuring amperage draw and using the last entered motor
+     * input, to determine if the wrist is going up or down / if it is all the way
+     * up or down. We can combine this with integrated encoder values to determine
+     * with some degree of accuracy of the wrist's position.
+     * 
+     * @return resistance in amps
+     */
+    public double getResistance() {
+        return wristRotation.getSupplyCurrent();
+    }
 
-        if(resistance < 4.8) {
-            wristDisabled = false;
+    /**
+     * Set the wrist output percent.
+     * Automatically stops the wrist if resistance is met over threshold:
+     * {@link WristRotationBase#resistanceTollerance}
+     * 
+     * @param percentOutput Negetive value moves the wrist outside and positive
+     *                      values move the wrist inside
+     */
+    public void setWrist(double percentOutput) {
+        if (getResistance() < resistanceTollerance) {
+            this.wristRotation.set(ControlMode.PercentOutput, percentOutput);
         } else {
-            wristDisabled = true;
-            wristRotation.set(ControlMode.PercentOutput, 0);
+            // wristRotation.set(ControlMode.PercentOutput, 0);
+            stopWrist();
         }
     }
 
     /**
-     * Return the resistance faced by the wrist motor,
-     * negetive value means the wrist is down, positive value means the wrist is
-     * inside the robot, if the value is non-zero,
-     * there is significant resistance
+     * Stop the wrist motor, will not run again till another input is given
      */
-    private double checkResistance() {
-        double resistance = getResistance();
-
-        if (lastInput == 0 || resistance < 5.1)
-            return 0;
-
-        return lastInput;
-
-    }
-
-    private double getResistance() {
-        return wristRotation.getSupplyCurrent();
-    }
-
-    public void setWrist(double percentOutput) {
-        if(wristDisabled) return;
-        lastInput = (int) Math.copySign(1, percentOutput);
-        this.wristRotation.set(ControlMode.PercentOutput, percentOutput);
-    }
-
     public void stopWrist() {
-        lastInput = 0;
         wristRotation.stopMotor();
     }
 }
