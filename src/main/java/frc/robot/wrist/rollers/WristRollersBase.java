a490e4005b5b6dba197e40e347ee2a6b4b19b518
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2022-10-21 22:48:37 -0400

    added datamap for roller amperage resistance

--- src/main/java/frc/robot/wrist/rollers/WristRollersBase.java
+++ src/main/java/frc/robot/wrist/rollers/WristRollersBase.java
@@ -1,13 +1,27 @@
 package frc.robot.wrist.rollers;
 
+import org.talon540.mapping.data.BoundDataMap;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.constants.CANDeviceIDS;
 
 public class WristRollersBase extends SubsystemBase {
     private WPI_TalonSRX wristRollers;
+
+    public BoundDataMap resistanceMap = new BoundDataMap(150);
+
+    @Override
+    public void periodic() {
+        addDataPoint(wristRollers.getSupplyCurrent());
+    }
+
+    public boolean peakFound() {
+        return (1.5 * resistanceMap.getStandardDeviation()) < wristRollers.getSupplyCurrent();
+    }
 
     public WristRollersBase() {
         this.wristRollers = new WPI_TalonSRX(CANDeviceIDS.INTAKE_ROLLERS);
@@ -19,5 +33,18 @@ public class WristRollersBase extends SubsystemBase {
 
     public void stopRollers() {
         this.wristRollers.stopMotor();
+    }
+
+    public void addDataPoint(double point) {
+        resistanceMap.addNode(point);
+    }
+
+    public void clearDataMap() {
+        resistanceMap.clearList();
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
+        builder.addBooleanProperty("Peak Found", this::peakFound, null);
     }
 }
